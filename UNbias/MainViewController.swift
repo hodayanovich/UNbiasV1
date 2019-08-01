417ca49be95f0de9becaf4f862e8ef146b6e465d
Daya Novich <dayanovich@Dayas-Air.home>
2019-08-01 19:22:15 -0400

    first time on ds laptop

---
+++ UNbias/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  UNbias
+//
+//  Created by Apple on 8/1/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // to set background image:
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "HomeScreen")!)
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
